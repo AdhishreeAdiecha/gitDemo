3a7fd4265d7f0980f4367b368741888c34f474bf
adhishree <adhishree.adiecha@brevitaz.com>
2018-01-05 10:30:16 +0530

    msg to arpy

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -1,6 +1,7 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hiii Adhishree...");
+        System.out.println("Hi arpy !! Good morning !");
     }
 
 }
